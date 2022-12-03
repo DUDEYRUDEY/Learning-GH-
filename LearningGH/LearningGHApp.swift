b537912b282fdd8db95e2fe2003788f564d92ead
DUDEYRUDEY <101237243+DUDEYRUDEY@users.noreply.github.com>
2022-12-03 20:51:00 +1100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Sean Smith on 3/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
